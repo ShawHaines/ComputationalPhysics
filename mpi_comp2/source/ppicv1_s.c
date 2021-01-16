4ebbd838260aeb59399dfb0de2c65b8b0b68c21f
Shaw Haines <haohaistone@qq.com>
2021-01-16 17:01:38 +0800

    Changed the program to a left-going wave.

--- mpi_comp2/source/ppicv1_s.c
+++ mpi_comp2/source/ppicv1_s.c
@@ -14,22 +14,23 @@ char *argv[];
 
 	MPI_Init(&argc,&argv);
 	MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
-	MPI_Comm_size(MPI_COMM_WORLD,&num_rank); // totally 8 ranks
+	MPI_Comm_size(MPI_COMM_WORLD,&num_rank); // 8 ranks in total.
 
 	if(my_rank==0)	printf(">> Total_rank_num= %d\n",num_rank);
 
 	I_rank=I/num_rank;
+	// Note that there's 2 guard cell each at emp[0] and emp[I_rank+1].
 	emp=dvector(0,I_rank+1);
 
 	for(i=0;i<=I_rank+1;i++) emp[i]=0.0;
 
-	//初始化三角波
-	if(my_rank==0){
+	// initialize the triangular wave.
+	if(my_rank==num_rank-1){
 		for(i=25;i<50;i++)  emp[i]=(i-25.0)/25.0;
 		for(i=50;i<=75;i++) emp[i]=1.0-(i-50.0)/25.0;
 	}
 
-	//输出初始值
+	// output the initial values
 	if(my_rank==0)	printf("n = %03d\n",n);
 	sprintf(file1,"data/emp_%03d_%d.txt",0,my_rank);
 	fp=fopen(file1,"w");
@@ -39,13 +40,13 @@ char *argv[];
 
 	for(n=1;n<=700;n++){
 
-		//向右移动
-		for(i=I_rank;i>=0;i--) 	emp[i+1]=emp[i];
+		// move leftwards
+		for(i=0;i<=I_rank;i++) 	emp[i]=emp[i+1];
 
-		//发送接收
+		// sending and receiving
 		SR();
 
-		//输出数据
+		// output data.
 		if(n%10==0){
 			if(my_rank==0)	printf("n = %03d\n",n);
 			sprintf(file1,"data/emp_%03d_%d.txt",n,my_rank);
